fc032e686bf06678874e34bc436461036eb8b722
Eric Rudisaile <erudisaile@microdesk.com>
2018-07-21 09:04:28 -0400

    pull out CFM and sizing into seperate file

--- pathing/minSpanningPath.py
+++ pathing/minSpanningPath.py
@@ -5,6 +5,8 @@ import ductSizing
 
 import math
 
+import CFMAndSizes
+
 tstbldg = [
    [
     { "name": "Space-101",
@@ -43,51 +45,51 @@ def GetRoutes(aLevel):
     return span
     
 
-#this adds the load for each point along all edges in the path
-def AddCFMToRoute(lvl, span):
-    # print("Level Info:", lvl)
-    shafts = [s for s in lvl if s["name"] == "Shaft"]
-    # print("Shafts: ", shafts)
-    shaftNode = -1
-    if len(shafts)>0:
-        # print ("found shaft node")
-        shaftNode = lvl.index(shafts[0])
-    else:
-        return span
-    for n in range(0,len(lvl)):
-        destSpace = lvl[n]
+# #this adds the load for each point along all edges in the path
+# def AddCFMToRoute(lvl, span):
+#     # print("Level Info:", lvl)
+#     shafts = [s for s in lvl if s["name"] == "Shaft"]
+#     # print("Shafts: ", shafts)
+#     shaftNode = -1
+#     if len(shafts)>0:
+#         # print ("found shaft node")
+#         shaftNode = lvl.index(shafts[0])
+#     else:
+#         return span
+#     for n in range(0,len(lvl)):
+#         destSpace = lvl[n]
         
-        if(n==shaftNode):
-            continue 
-        pth = next(nx.shortest_simple_paths(span, source=lvl[shaftNode]['id'], target=destSpace['id']))
-        # print('found path')
-        for i in range(1,len(pth)):
-            # print("i: ", i)
-            if loadVar not in span[pth[i-1]][pth[i]]:
-                span[pth[i-1]][pth[i]][loadVar] = 0
-            span[pth[i-1]][pth[i]][loadVar] += destSpace['cfm']
-            # print(span[pth[i-1]][pth[i]])
+#         if(n==shaftNode):
+#             continue 
+#         pth = next(nx.shortest_simple_paths(span, source=lvl[shaftNode]['id'], target=destSpace['id']))
+#         # print('found path')
+#         for i in range(1,len(pth)):
+#             # print("i: ", i)
+#             if loadVar not in span[pth[i-1]][pth[i]]:
+#                 span[pth[i-1]][pth[i]][loadVar] = 0
+#             span[pth[i-1]][pth[i]][loadVar] += destSpace['cfm']
+#             # print(span[pth[i-1]][pth[i]])
 
-    return span
+#     return span
 
-def AddSizesToRoute(span):
-    for e in span.edges:
-        # print(e)
-        edge = span[e[0]][e[1]]
-        cfm = edge['cfm']
-        if cfm > 0:
-            ed = ductSizing.calcEDofPDandFlow(cfm, 0.08)
-        else:
-            ed = 1
-        h = math.floor(ed)
-        w = math.ceil(ductSizing.calcSecondDimension(ed, h))
-        edge["width"] =  w * 25.4
-        edge["height"] =  h * 25.4
+# def AddSizesToRoute(span):
+#     for e in span.edges:
+#         # print(e)
+#         edge = span[e[0]][e[1]]
+#         cfm = edge['cfm']
+#         if cfm > 0:
+#             ed = ductSizing.calcEDofPDandFlow(cfm, 0.08)
+#         else:
+#             ed = 1
+#         h = math.floor(ed)
+#         w = math.ceil(ductSizing.calcSecondDimension(ed, h))
+#         edge["width"] =  w * 25.4
+#         edge["height"] =  h * 25.4
 
-    return span
+#     return span
 
 # shaftnode = 0
-loadVar = 'cfm'
+
 
 def EdgesToDict(G):
     myducts = []
@@ -101,8 +103,8 @@ def GetDuctPathFromBldg(bldg):
     for lvl in bldg:
         spanningTree = GetRoutes(lvl)
         # print("SpanningTree:", spanningTree)
-        spanWithLoads = AddCFMToRoute(lvl, spanningTree)
-        spanWithSize = AddSizesToRoute(spanWithLoads)
+        spanWithLoads = CFMAndSizes.AddCFMToRoute(lvl, spanningTree)
+        spanWithSize = CFMAndSizes.AddSizesToRoute(spanWithLoads)
         allDucts.extend( EdgesToDict(spanWithSize) )
 
     return allDucts
