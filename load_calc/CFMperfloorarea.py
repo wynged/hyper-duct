47f708925b81e0f38ea59d65a189892af5c62736
patpease <patpease0@gmail.com>
2018-07-20 23:03:56 -0400

    added cfmperfloorarea

---
+++ load_calc/CFMperfloorarea.py
@@ -0,0 +1,10 @@
+CFM_per_FloorArea = {
+    "Shaft":0,
+    "Office":1,
+    "Conference": 1.5,
+    "Bathroom":.5,
+    "Lobby":1.25
+    "Kitchen":3,
+    "Incubator":2.5
+    "Elevator lobby":1.25
+}
