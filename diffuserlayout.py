04e148353759265829c209742bdb9da7b7ba8221
natgmac <nathaniel.g.macdonald@gmail.com>
2018-07-21 09:00:05 -0400

    difflay

--- diffuserlayout.py
+++ diffuserlayout.py
@@ -10,10 +10,23 @@ from aecSpace.aecSpacer import aecSpacer
 from aecSpace.aecSpaceGroup import aecSpaceGroup
 from aecSpace.aecSpaceDrawOCC import aecSpaceDrawOCC
 import itertools
+import hypar
 
 spaces = MakeSpaceTower.makeSpaceTower()
 space_ceilings = []
 #print(spaces)
 for space in spaces:
-    space_ceilings.append(space.mesh_ceiling)
+    space_ceilings.append(space.getMesh2D)
 print(space_ceilings[0])
+
+
+'''
+r = 3
+c = 4
+
+x = [i % c for i in range(r*c)]
+y = [i / c for i in range(r*c)]
+
+print x
+print y
+'''
