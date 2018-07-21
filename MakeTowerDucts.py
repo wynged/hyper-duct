9b43b596f68a651da2982d4ed6af1af2d1db7608
Eric Rudisaile <erudisaile@microdesk.com>
2018-07-21 10:45:44 -0400

    modify duct height to have a z offset

--- MakeTowerDucts.py
+++ MakeTowerDucts.py
@@ -83,10 +83,11 @@ def makeTowerDucts(stories: int = 5, mostRooms: int = 4, routing = 0, useColor=0
         r,g,b = heatmap.convert_to_rgb(mini, maxi, dI)
         hmColors.append(model.add_material(r/255,g/255,b/255,1.0,1.0,"HM"+str(i)))
     for ductSpec in ductSpecs:
+        # print(ductSpec)
         start = aecPoint(ductSpec['start'][0], ductSpec['start'][1], ductSpec['start'][2])
         end = aecPoint(ductSpec['end'][0], ductSpec['end'][1], ductSpec['end'][2])
         chosenColor = math.floor(12-ductSpec['cfm'] / (maxi-mini) * 12 )
-        duct = MakeDuct.makeDuct(start, end, ductSpec['width'], ductSpec['height'])
+        duct = MakeDuct.makeDuct(start, end, ductSpec['height'], ductSpec['width'])
         ductMesh = duct.mesh_graphic
         if useColor == 1:
             model.add_triangle_mesh(ductMesh.vertices, ductMesh.normals, ductMesh.indices, hmColors[ chosenColor-1])   
--- pathing/minSpanningPath.py
+++ pathing/minSpanningPath.py
@@ -44,53 +44,6 @@ def GetRoutes(aLevel):
     span = nx.minimum_spanning_tree(G)
     return span
     
-
-# #this adds the load for each point along all edges in the path
-# def AddCFMToRoute(lvl, span):
-#     # print("Level Info:", lvl)
-#     shafts = [s for s in lvl if s["name"] == "Shaft"]
-#     # print("Shafts: ", shafts)
-#     shaftNode = -1
-#     if len(shafts)>0:
-#         # print ("found shaft node")
-#         shaftNode = lvl.index(shafts[0])
-#     else:
-#         return span
-#     for n in range(0,len(lvl)):
-#         destSpace = lvl[n]
-        
-#         if(n==shaftNode):
-#             continue 
-#         pth = next(nx.shortest_simple_paths(span, source=lvl[shaftNode]['id'], target=destSpace['id']))
-#         # print('found path')
-#         for i in range(1,len(pth)):
-#             # print("i: ", i)
-#             if loadVar not in span[pth[i-1]][pth[i]]:
-#                 span[pth[i-1]][pth[i]][loadVar] = 0
-#             span[pth[i-1]][pth[i]][loadVar] += destSpace['cfm']
-#             # print(span[pth[i-1]][pth[i]])
-
-#     return span
-
-# def AddSizesToRoute(span):
-#     for e in span.edges:
-#         # print(e)
-#         edge = span[e[0]][e[1]]
-#         cfm = edge['cfm']
-#         if cfm > 0:
-#             ed = ductSizing.calcEDofPDandFlow(cfm, 0.08)
-#         else:
-#             ed = 1
-#         h = math.floor(ed)
-#         w = math.ceil(ductSizing.calcSecondDimension(ed, h))
-#         edge["width"] =  w * 25.4
-#         edge["height"] =  h * 25.4
-
-#     return span
-
-# shaftnode = 0
-
-
 def EdgesToDict(G):
     myducts = []
     for e in G.edges:
@@ -104,7 +57,7 @@ def GetDuctPathFromBldg(bldg):
         spanningTree = GetRoutes(lvl)
         # print("SpanningTree:", spanningTree)
         spanWithLoads = CFMAndSizes.AddCFMToRoute(lvl, spanningTree)
-        spanWithSize = CFMAndSizes.AddSizesToRoute(spanWithLoads)
+        spanWithSize = CFMAndSizes.AddSizesToRoute(spanWithLoads, -2)
         allDucts.extend( EdgesToDict(spanWithSize) )
 
     return allDucts
