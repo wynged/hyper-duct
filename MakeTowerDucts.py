de4b0c1bcba30242a4d5eaa717b7f0e0969dd235
Eric Rudisaile <erudisaile@microdesk.com>
2018-07-21 02:40:33 -0400

    include the load thing
    inclu ethe load thing at the beginning

--- MakeTowerDucts.py
+++ MakeTowerDucts.py
@@ -4,6 +4,7 @@ import MakeDuct
 import MakeSpaceTower
 import ductSizing
 import minSpanningPath
+import space_cfm_calc
 from aecSpace.aecColor import aecColor
 from aecSpace.aecFloor import aecFloor
 from aecSpace.aecShaper import aecShaper
@@ -14,18 +15,24 @@ from aecSpace.aecSpacer import aecSpacer
 from aecSpace.aecSpaceDrawOCC import aecSpaceDrawOCC
 
 def makeTowerDucts():
-    # spaces = MakeSpaceTower.makeSpaceTower()
-    ductSpecs = minSpanningPath.GetDuctPathFromBldg()
+    spaces = MakeSpaceTower.makeSpaceTower()
+    # print(spaces)
+    loads = space_cfm_calc.Space_CFM_Calc(spaces)
+    print(loads[0])
+    ductSpecs = minSpanningPath.GetDuctPathFromBldg(loads[0])
     ducts = []
+    print(ductSpecs)
     for ductSpec in ductSpecs:
+        print(ductSpec)
         start = aecPoint(ductSpec['start'][0], ductSpec['start'][1], ductSpec['start'][2])
         end = aecPoint(ductSpec['end'][0], ductSpec['end'][1], ductSpec['end'][2])
         ducts += [MakeDuct.makeDuct(start, end, ductSpec['width'], ductSpec['height'])]
     if len(ducts) == 1: ducts = [ducts]
     return ducts
     
-spaces = makeTowerDucts()
-spaceDrawer = aecSpaceDrawOCC()
-spaceDrawer.draw3D(spaces, displaySize = (1600, 900), update = True)
+ducts = makeTowerDucts()
+# spaceDrawer = aecSpaceDrawOCC()
+# spaceDrawer.draw3D(spaces, displaySize = (1600, 900), update = True)
 # update = True animates the example by updating the display after every space placement.
 # About 60x slower to completion, but more interesting to watch.
+
